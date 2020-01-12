c5696eca07547874f9e46793b0dcc1fee06c7fe1
WarzouMc <49000416+WarzouMc@users.noreply.github.com>
2020-01-12 20:07:43 +0100

    Update NumbersSeparator.java

--- NumbersSeparator.java
+++ NumbersSeparator.java
@@ -2,15 +2,37 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class consist to transform number in String with better number readability.
+ * @author WarzouMc
+ * @version 2.0
+ */
 public class NumbersSeparator {
-
+    /**
+     * Default separator character
+     */
     private char separator = ' ';
+
+    /**
+     * Call class whit default separator character
+     */
+    public NumbersSeparator(){}
+
+
+    /**
+     * @param separator
+     * Call class whit custom separator character
+     */
     public NumbersSeparator(char separator){
         this.separator = separator;
     }
 
-    public NumbersSeparator(){}
-
+    /**
+     * @param i
+     * This method return a String after have do a separation every 3 number for a int type number
+     * 154861345 -> 154 861 345
+     * @return
+     */
     public String transformer(int i) {
         String intToString = Integer.toString(i);
         int length = intToString.length();
@@ -30,6 +52,12 @@ public class NumbersSeparator {
         return returnString.toString();
     }
 
+    /**
+     * @param l
+     * This method return a String after have do a separation every 3 number for a long type number
+     * 1215348361583515635186515 -> 1 215 348 361 583 515 635 186 515
+     * @return
+     */
     public String transformer(long l) {
         String longToString = Long.toString(l);
         int length = longToString.length();
@@ -48,5 +76,4 @@ public class NumbersSeparator {
         characterList.forEach(returnString::append);
         return returnString.toString();
     }
-
 }
